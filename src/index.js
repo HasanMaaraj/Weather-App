f416870f1fb2715b8cbbfd9d0994abf9e47dc26e
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2023-08-08 08:40:08 +0300

    Obtain request data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './style.css';
+
+async function getWeather() {
+    const weatherRequest = await fetch('http://api.weatherapi.com/v1/current.json?key=cb91ec8608124e5c83750430230708&q=London&aqi=yes', {
+        mode: 'cors',
+    });
+    const weatherData = await weatherRequest.json()
+    return weatherData
+}
+
+console.log(await getWeather());
